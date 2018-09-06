da85bf01fd42940d4ff1d85118938a5d3c4a079b
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2018-09-06 14:21:10 +0300

    Implemented the cache cleanup plugin (CleanupCache.csproj)
    (cherry picked from commit ba6e5c33e75d9f27feb45a1df1f8cb048f0b9deb)
    
    # Conflicts:
    #	GitExtensionsMono.sln
    #	Setup/MakePortableArchive.cmd

---
+++ Plugins/CleanupCache/CleanupCachePlugin.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using CleanupCache.Properties;
+using GitUIPluginInterfaces;
+using ResourceManager;
+
+namespace CleanupCache
+{
+    [Export(typeof(IGitPlugin))]
+    public class CleanupCachePlugin : GitPluginBase, IGitPluginForRepository
+    {
+        public CleanupCachePlugin()
+        {
+            SetNameAndDescription("Clean up the cache");
+            Translate();
+        }
+
+        public override bool Execute(GitUIBaseEventArgs gitUiCommands)
+        {
+            gitUiCommands.GitModule.RunGitCmd("rm -r --cached .");
+            gitUiCommands.GitModule.RunGitCmd("add .");
+            return true;
+        }
+    }
+}
